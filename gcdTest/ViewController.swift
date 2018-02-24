e730e81d0d63dfa8c401b99b1ec3d1238a35df74
Liber <298038759@qq.com>
2018-02-24 21:13:08 +0900

    delay

--- gcdTest/ViewController.swift
+++ gcdTest/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    typealias Task = (_ cancel: Bool) -> Void
+    
+    func delay(_ time: TimeInterval, task:@escaping ()->()) -> Task? {
+        func dispatch_later(block:@escaping ()->() ) {
+            let t = DispatchTime.now() + time
+            DispatchQueue.main.asyncAfter(deadline: t, execute: block)
+        }
+        var closure: (()->Void)? = task
+        var result: Task?
+        let delayedClosure: Task = {
+            cancel in
+            if let internalClosure = closure {
+                if (cancel == false) {
+                    DispatchQueue.main.async(execute: internalClosure)
+                }
+            }
+            closure = nil
+            result = nil
+        }
+        result = delayedClosure
+        dispatch_later {
+            if let delayedClosure = result {
+                delayedClosure(false)
+            }
+        }
+        return result
+    }
+    func cancel(_ task: Task?) {
+        task?(true)
+    }
+    @IBAction func click(_ sender: Any) {
+        delay(2) { print("2秒后输出" ) }
+        //        let task = delay(5) {
+        //            print("打110")
+        //        }
+        //        cancel(task)
+    }
 }
 
